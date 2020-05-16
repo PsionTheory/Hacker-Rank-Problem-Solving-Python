504dcc2ea53f6b898ddd103cb2043ba67cb45abe
Meet Gandhi <meet.gandhi1415@gmail.com>
2020-05-16 19:58:48 +0530

    Commit

---
+++ Implimentation/Birthday Chocolate.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthday function below.
+def birthday(s, d, m):
+    c=0
+    for i in range(0,((len(s)-m)+1)):
+        total=s[i]
+        for j in range(1,m):
+            total=total+s[i+j]
+        if total==d:
+            c+=1
+    return(c)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    dm = input().rstrip().split()
+
+    d = int(dm[0])
+
+    m = int(dm[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
