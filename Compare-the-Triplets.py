f9a77111de83c082a2221a6ff4d41c19d33c187b
Juhi Kamdar <43910571+thewhiteflower110@users.noreply.github.com>
2020-05-16 18:16:06 +0530

    Create Compare-the-Triplets.py

---
+++ Compare-the-Triplets.py
@@ -0,0 +1,37 @@
+#Compare the Triplets
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    alice=0
+    bob=0
+    for i,j in zip(a,b):
+        if i>j:
+            alice+=1
+        if j>i:
+            bob+=1
+    l=[]
+    l.append(alice)
+    l.append(bob)
+    return l
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
