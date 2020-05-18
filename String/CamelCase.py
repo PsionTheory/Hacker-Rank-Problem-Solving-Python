a339b1e8d51876d1ec7d7a9ae5448842e48ba967
Juhi Kamdar <43910571+thewhiteflower110@users.noreply.github.com>
2020-05-18 22:08:19 +0530

    Create CamelCase.py

---
+++ String/CamelCase.py
@@ -0,0 +1,27 @@
+#CamelCase
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the camelcase function below.
+def camelcase(s):
+    count=0
+    for i in s:
+        if ord(i)>=65 and ord(i)<=90:
+            count+=1
+    return count+1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
